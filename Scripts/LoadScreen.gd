41488ad069a5d46f792005814bbe9461fa6c315d
LMN <43175590+nekoumyu@users.noreply.github.com>
2022-08-14 12:52:11 +0200

    changes hover activity on start and retry button

--- Scripts/LoadScreen.gd
+++ Scripts/LoadScreen.gd
@@ -2,19 +2,28 @@ extends Control
 
 onready var button = $"%Button"
 onready var sprite = $Button/Sprite
+onready var spriteScaleXZoom = sprite.scale.x+0.05
+onready var spriteScaleYZoom = sprite.scale.y+0.05
 
 var sprite_pos
 
 func _ready():
 	sprite_pos = sprite.position
+	sprite.modulate.a = 0.8
 
 func _process(_delta):
 	if button.is_hovered():
-		sprite.modulate.a = 0.8
-		sprite.position.y = sprite_pos.y - 10
-	else:
 		sprite.modulate.a = 1
+		sprite.scale.x = spriteScaleXZoom
+		sprite.scale.y = spriteScaleYZoom
+		
+	else:
 		sprite.position = sprite_pos
+		sprite.scale.x = spriteScaleXZoom-0.05
+		sprite.scale.y = spriteScaleYZoom-0.05
+		sprite.modulate.a = 0.8
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Scenes/Game.tscn")
+
+
